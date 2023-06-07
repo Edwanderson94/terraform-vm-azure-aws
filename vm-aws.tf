97c4a93f3fe6500f09caae19720c2201cad8839e
unknown <edi171194@gmail.com>
2023-06-06 21:53:42 -0300

    adição da variavel ARM_CLIENT_ID

--- vm-aws.tf
+++ vm-aws.tf
@@ -6,9 +6,9 @@ resource "aws_key_pair" "key" {
 resource "aws_instance" "vm" {
   ami                         = "ami-0af6e9042ea5a4e3e" # Linux 22.04 LTS
   instance_type               = "t3.nano" # Na zona de disponibilidade sa-east-1b não suporta maquinas t2
-  availability_zone           = "sa-east-1a" #Em fase de teste
+  # availability_zone           = "sa-east-1a" #Em fase de teste
   key_name                    = aws_key_pair.key.key_name
-  subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id # Utilizando o data source do bucket s3 da VPC 
+  subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id # Utilizando o data source do bucket s3 da VPC
   vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
   associate_public_ip_address = true
 
